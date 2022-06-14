bd17de76f6643c90f6b6b73d5d5d4f5f2e952509
Shubham Sarda <shubhamsarda00@gmail.com>
2022-06-14 06:55:50 +0530

    Add files via upload

---
+++ Assignment5/src/LLMergeSort.java
@@ -0,0 +1,117 @@
+package col106.assignment5;
+
+import java.util.Comparator;
+
+/*
+Implementation of MergeSort Algorithm :
+    1. you get linked list of size <=1 you just return the list as it is already sorted
+    2. Find mid node using findSplit method(Don't forget to add mid element to globalList before returning it)
+    3. Create two LinkedList lt (with head = head and tail = mid) and rt (with head = mid.next and tail = tail)
+    4. Now recursively MergSort lt and rt Linked lists(Maintain this order)
+    5. Now merge these two lists that we got from recursive calls using given crieteria for ordering
+    6. Return merged Linked list
+*/
+
+public class LLMergeSort <T extends Comparable>  {
+
+  LinkedList<T>  globalList = new LinkedList<T>();
+
+  //CALL THIS METHOD AFTER EVERY CALL OF findSplit and DO NOT MODIFY THIS METHOD
+  public void adjustGlobalPointer(T node){
+      globalList.add(node);
+  }
+
+  // Utility function to get the middle of the linked list
+  public Node<T> findSplit(LinkedList<T>  lst) {
+    //find middle node of LL :
+	  int n=lst.getSize();
+	  int m =midsize(n);
+	  Node<T> h= lst.getHead();
+	  for(int i=0;i<m-1;i++) {
+		  h=h.getNext();
+	  }
+      Node<T> middle = h;
+    //Enter your code here
+
+    //!!!!!*****DO NOT REMOVE THIS METHOD CALL (change the argument apprpriately)*****!!!!!
+    adjustGlobalPointer(middle.getData()); //Add object of ItemNode after finding mid in each call
+    return middle;
+  }
+
+
+  public LinkedList<T>  MergeSort(LinkedList<T>  lst) {
+	  
+    //Recursively Apply MergeSort, by calling function findSplit(..) to find middle node to split
+    //Enter your code here
+	  if(lst.getSize()<=1) {
+		  return lst;
+	  }
+	  Node<T> middle=findSplit(lst);
+	  
+	  Node<T> nex=middle.getNext();
+	  middle.next=null;
+	  int s=lst.getSize();
+	  int m=midsize(s);
+	  lst.setSize(m);
+	  LinkedList<T> left=MergeSort(lst);
+	  LinkedList<T> temp = new LinkedList<T>();
+	  temp.setHead(nex);
+	  temp.setSize(s-m);
+	  LinkedList<T> right=MergeSort(temp);
+	  LinkedList<T> sorted = merge(left,right);
+	  return sorted;
+	  
+  }
+  private LinkedList<T> merge(LinkedList<T> l1, LinkedList<T> l2){
+	  LinkedList<T> sorted = new LinkedList<T>();
+	  Node<T> h1=l1.getHead();
+	  Node<T> h2=l2.getHead();
+	  while(h1!=null && h2!=null) {
+		  if(h1.getData().compareTo(h2.getData())>0) {
+			  
+			  sorted.add(h2.getData());
+			  h2=h2.getNext();
+		  }
+		  else {
+			  sorted.add(h1.getData());
+			  h1=h1.getNext();
+		  }
+		  
+	  }
+	  if(h1==null) {
+		  while(h2!=null) {
+			  sorted.add(h2.getData());
+			  h2=h2.getNext();
+		  }
+		  
+	  }
+	  else {
+		  while(h1!=null) {
+			  sorted.add(h1.getData());
+			  h1=h1.getNext();
+		  }
+	  }	  
+	  return sorted;
+	  
+  }
+  public int midsize(int n) {
+	  
+	  if(n%2==0) {
+		  return n/2; 
+	  }
+	  else {
+		  return (n-1)/2+1;
+	  }
+	  
+  }
+
+  //DO NOT CALL OR MODIFY THESE METHODS IN YOUR CALL THIS IS FOR USE IN DRIVER CODE
+  public LinkedList<T> getGlobalList() {
+    return this.globalList;
+  }
+
+  public void clearGlobalList(){
+    globalList  = new LinkedList<>();
+  }
+
+}
---
+++ Assignment5/src/StockMgmt.java
@@ -0,0 +1,290 @@
+package col106.assignment5;
+
+
+public class StockMgmt implements StockMgmtInterface {
+	//!!!!!!!*********DON'T CREATE YOUR OWN OBJECTS OF LLMergeSort*********!!!!!!!
+	//use these only this object to sort
+	LLMergeSort mergeSortobj;
+
+	LinkedList<CategoryNode> categories;
+	/*public static void main(String[] args) {
+		
+		/*
+		DateNode d1= new DateNode(1,1,2000);
+		DateNode d2= new DateNode(1,1,2000);
+		ItemNode i1= new ItemNode(11,"item21",20);
+		ItemNode i2= new ItemNode(12,"item21",20);
+		i1.addTransaction(new PurchaseNode(1,1,1,2000));
+		i1.addTransaction(new PurchaseNode(1,1,1,2001));
+		i2.addTransaction(new PurchaseNode(1,1,1,2000));
+		i2.addTransaction(new PurchaseNode(1,1,1,2008));
+		i2.addTransaction(new PurchaseNode(1,1,1,2038));
+		System.out.println(i2.latest.year);
+		
+		System.out.println(i1.compareTo(i2));
+		*/
+	
+		/*
+		StockMgmt s = new StockMgmt();
+		s.addItem(1, 1, "item11", 100);
+		s.addItem(1, 1, "item12", 100);
+		s.addItem(2, 1, "item21", 100);
+		s.addItemTransaction(1, 1, "item11", 10, 10, 3, 2006);
+		s.addItemTransaction(1, 1, "item11", 10, 25, 1, 1998);
+		s.addItemTransaction(1, 2, "item12", 10, 10, 3, 2006);
+		s.addItemTransaction(2, 1, "item21", 10, 25, 1, 2008);
+		LinkedList<ItemNode> sort=s.sortByPurchasePeriod(10, 3, 2006, 10, 3, 2006);
+		Node<ItemNode> current = sort.getHead();
+	    
+	    while (current != null) {
+	      System.out.println(current.data.itemId+" " +current.data.itemName+" " +current.data.stock);
+	      current = current.getNext();
+	    }
+		*/
+		/*
+		StockMgmt s = new StockMgmt();
+		s.addItem(1, 1, "item11", 100);
+		s.addItem(1, 1, "item12", 250);
+		s.addItem(2, 1, "item21", 100);
+		s.addItem(2, 2, "item22", 300);
+		s.addItem(3, 1, "item31", 200);
+		s.addItem(3, 2, "item32", 260);
+		s.addItem(3, 3, "item33", 500);
+		s.addItem(4, 1, "item41", 1000);
+		s.addItem(4, 2, "item42", 400);
+		s.addItem(5, 1, "item51", 300);
+		s.addItemTransaction(1, 1, "item11", 10, 10, 3, 2000);
+		s.addItemTransaction(1, 2, "item12", 20, 20, 6, 2005);
+		s.addItemTransaction(2, 1, "item21", 15, 25, 1, 1998);
+		s.addItemTransaction(2, 2, "item22", 35, 16, 10, 1997);
+		s.addItemTransaction(3, 1, "item31", 20, 9, 12, 1996);
+		s.addItemTransaction(3, 3, "item33", 30, 16, 2, 1998);
+		s.addItemTransaction(3, 2, "item32", 5, 2, 5, 2010);
+		s.addItemTransaction(4, 1, "item41", 6, 18, 7, 1997);
+		s.addItemTransaction(4, 2, "item42", 10, 26, 9, 2009);
+		s.addItemTransaction(5, 1, "item51", 25, 6, 11, 2006);
+		s.addItemTransaction(1, 2, "item12", 15, 1, 10, 2000);
+		s.addItemTransaction(3, 3, "item33", 50, 26, 5, 2016);
+		s.addItemTransaction(4, 1, "item41", 8, 10, 7, 1998);
+		s.addItemTransaction(4, 2, "item42", 12, 26, 8, 1999);
+		s.addItemTransaction(5, 1, "item51", 21, 6, 10, 2008);
+		s.addItemTransaction(3, 2, "item32", 5, 28, 8, 2011);
+		s.addItemTransaction(2, 2, "item22", 5, 16, 1, 2010);
+		s.addItemTransaction(1, 1, "item11", 16, 10, 5, 1995);
+		//LinkedList<ItemNode> sort=s.sortByLastDate();
+		LinkedList<ItemNode> sort=s.sortByPurchasePeriod(1, 1, 2000, 11, 6, 2020);
+		Node<ItemNode> current = sort.getHead();
+	    
+	    while (current != null) {
+	      System.out.println(current.data.itemId+" " +current.data.itemName+" " +current.data.stock);
+	      current = current.getNext();
+	    }
+	    */
+	    /*
+	    
+	    LinkedList<ItemNode> l= s.checkMergeSort();
+	    Node<ItemNode> curr = l.getHead();
+	    
+	    while (curr != null) {
+	      System.out.println(curr.data.itemId+" " +curr.data.itemName+" " +curr.data.stock);
+	      curr = curr.getNext();
+	    }
+		*/
+		
+		
+		
+	//}
+	//DO NOT MNODIFY THIS CONSTRUCTOR
+	public StockMgmt() {
+
+		mergeSortobj = new LLMergeSort();
+		categories = new LinkedList<CategoryNode>();
+		
+		categories.add(new CategoryNode(1, "mobile"));
+		categories.add(new CategoryNode(2, "utensils"));
+		categories.add(new CategoryNode(3, "sanitary"));
+		categories.add(new CategoryNode(4, "medicalEquipment"));
+		categories.add(new CategoryNode(5, "clothes"));
+		
+	}
+
+	public void addItem(int categoryId, int itemId, String itemName, int stock) {
+		//Your code goes here
+		ItemNode n = new ItemNode(itemId, itemName, stock);
+		Node<CategoryNode> head= categories.getHead();
+		
+		for(int i=0;i<categoryId-1;i++) {
+			head=head.getNext();				
+		}
+		
+		/*while(head.getData().categoryId!=categoryId ) {
+			head=head.next;
+		}
+		*/
+		head.getData().getLinkedListOfCategory().add(n);
+		
+		
+				
+				
+	}
+
+	public void addItemTransaction(int categoryId, int itemId, String itemName, int numItemPurchased, int day, int month, int year) {
+		//Your code goes here
+		Node<CategoryNode> head= categories.getHead();
+		
+		for(int i=0;i<categoryId-1;i++) {
+			head=head.getNext();				
+		}
+		Node<ItemNode> n= head.getData().getLinkedListOfCategory().getHead();
+		ItemNode i= n.getData();
+				
+		while(n!=null && i.getItemId()!=itemId && i.getItemName()!=itemName) {
+			n=n.getNext();
+			i=n.getData();
+		}
+		i.addTransaction(new PurchaseNode(numItemPurchased, day, month, year));
+		
+	}
+
+	//Query 1
+	public LinkedList<ItemNode> sortByLastDate() {
+		//Your code goes here
+		
+		LinkedList<ItemNode> l = new LinkedList<ItemNode>();
+		Node<CategoryNode> c=this.categories.getHead();
+			
+		while(c!=null) {
+			
+			Node<ItemNode> in = c.getData().itemList.getHead();
+			while(in!=null) {
+				ItemNode i=new ItemNode(in.data.itemId,in.getData().itemName,in.getData().stock);
+				i.key=1;
+				i.purchaseTransactions=in.getData().purchaseTransactions;
+				i.latest=in.data.latest;
+				i.num_of_trans=in.data.num_of_trans;
+				l.add(i);
+				in=in.getNext();
+			}
+			c=c.getNext();
+		}
+		l=mergeSortobj.MergeSort(l);
+		return l;
+		
+	}
+
+	//Query 2
+	public LinkedList<ItemNode> sortByPurchasePeriod(int day1, int month1, int year1, int day2, int month2, int year2) {
+		//Your code goes here
+		DateNode start=new DateNode(day1,month1,year1);
+		DateNode end=new DateNode(day2,month2,year2);
+		LinkedList<ItemNode> l = new LinkedList<ItemNode>();
+		Node<CategoryNode> c=this.categories.getHead();
+			
+		while(c!=null) {
+			
+			Node<ItemNode> in = c.getData().itemList.getHead();
+			while(in!=null) {
+				ItemNode i=new ItemNode(in.data.itemId,in.getData().itemName,in.getData().stock);
+				i.key=2;
+				i.purchaseTransactions=in.getData().purchaseTransactions;
+				
+				if(in.data.num_of_trans==0) {
+					i.key2=0;
+				}
+				else {
+					Node<PurchaseNode> pn = i.purchaseTransactions.getHead();
+					PurchaseNode p=pn.getData();
+					float numpurchases=0;
+					int firstyear=Integer.MAX_VALUE;
+					int lastyear=Integer.MIN_VALUE;
+					boolean b=false;		
+					while(pn!=null) {
+						p=pn.getData();
+						DateNode d=p.dateobj;
+						if(d.compareTo(start)>=0 && d.compareTo(end)<=0) {
+							b=true;
+							numpurchases+=p.numItemPurchased;
+							if(d.year>lastyear) {
+								lastyear=d.year;
+							}
+							if(d.year<firstyear) {
+								firstyear=d.year;
+							}
+							
+						}
+						pn=pn.getNext();
+					}
+					if(b) {
+					i.key2=numpurchases/(lastyear-firstyear+1);
+					}
+					else {
+						i.key2=0;
+					}
+				}
+				
+				i.latest=in.data.latest;
+				i.num_of_trans=in.data.num_of_trans;
+				l.add(i);
+				in=in.getNext();
+			}
+			c=c.getNext();
+		}
+		l=mergeSortobj.MergeSort(l);
+		return l;
+		
+		
+	}
+
+	//Query 3
+	public LinkedList<ItemNode> sortByStockForCategory(int category) {
+		//Your code goes here
+		LinkedList<ItemNode> l = new LinkedList<ItemNode>();
+		Node<CategoryNode> c=this.categories.getHead();
+		for(int i=0;i<category-1;i++) {
+			c=c.getNext();				
+		}
+		Node<ItemNode> in = c.getData().itemList.getHead();
+		while(in!=null) {
+			ItemNode i=new ItemNode(in.data.itemId,in.getData().itemName,in.getData().stock);
+			i.key=3;
+			i.purchaseTransactions=in.getData().purchaseTransactions;
+			i.latest=in.data.latest;
+			i.num_of_trans=in.data.num_of_trans;
+			l.add(i);
+			in=in.getNext();
+		}
+		
+		l=mergeSortobj.MergeSort(l);
+		return l;
+	}
+
+	//Query 4
+	public LinkedList<ItemNode> filterByCategorySortByDate(int category) {
+		//Your code goes here
+		LinkedList<ItemNode> l = new LinkedList<ItemNode>();
+		Node<CategoryNode> c=this.categories.getHead();
+		for(int i=0;i<category-1;i++) {
+			c=c.getNext();				
+		}
+		Node<ItemNode> in = c.getData().itemList.getHead();
+		while(in!=null) {
+			ItemNode i=new ItemNode(in.data.itemId,in.getData().itemName,in.getData().stock);
+			i.key=4;
+			i.purchaseTransactions=in.getData().purchaseTransactions;
+			i.latest=in.data.latest;
+			i.num_of_trans=in.data.num_of_trans;
+			l.add(i);
+			in=in.getNext();
+		}
+		
+		l=mergeSortobj.MergeSort(l);
+		return l;
+	}
+
+	//!!!!!*****DO NOT MODIFY THIS METHOD*****!!!!!//
+	public LinkedList<ItemNode> checkMergeSort() {
+		LinkedList<ItemNode> retLst = mergeSortobj.getGlobalList();
+		mergeSortobj.clearGlobalList();
+		return retLst;
+	}
+}
